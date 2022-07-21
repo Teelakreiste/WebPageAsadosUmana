324aa81f3b354da4d530fff9b765e8c768f7162d
Teelakreiste <71708924+Teelakreiste@users.noreply.github.com>
2022-07-21 00:29:23 -0500

    Token de acceso

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+    //Ingrese el tokern aqui
+    Swal.fire({
+      title: 'Ingrese el token',
+      input: 'text',
+      inputPlaceholder: 'Ingrese el token',
+      background: '#212121',
+      color: '#928c8c',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.value === 'aO3L9M&lhiNJR526rk') {
+        this.router.navigate(['/panel/administracion/crear/articulos']);
+        Swal.fire({
+          title: 'Token correcto',
+          text: 'El token es correcto',
+          icon: 'success',
+          background: '#212121',
+          color: '#928c8c',
+          confirmButtonText: 'Ok',
+          confirmButtonColor: '#F15D11'
+        });
+        this.router.navigate(['panel/administracion/mostrar/articulos']);
+      }
+    });
+  }
+
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyAS4daJj6ZnOlMGvFrUyeGjuRVjBhqnmGM",
+  authDomain: "crud-au.firebaseapp.com",
+  projectId: "crud-au",
+  storageBucket: "crud-au.appspot.com",
+  messagingSenderId: "949380540709",
+  appId: "1:949380540709:web:6c55061a90e2db09c092cc"
+  }
 };
